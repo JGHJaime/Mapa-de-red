dd19b7425968116f061694e5123bde86022a0e90
JghJaime <palomojr01@gmail.com>
2024-07-04 13:54:03 +0200

    lista de coches bien hecha

--- coches.js
+++ coches.js
@@ -1,2 +1,20 @@
 const coches = require('./coches.json');
-console.log(coches);
+//console.log(coches[0]);
+let imprimir = (coche) => {
+console.log("------------------------------")
+console.log(coche.marca)
+console.log(coche.modelo)
+console.log("Nombre: " + coche.modelo)
+console.log("Marca: " + coche.marca)
+console.log("Color: " + coche.color)
+console.log("Año: " + coche.año)
+}
+imprimir(coches[0]);
+
+coches.forEach(coche => {
+        imprimir(coche)
+    }
+)
+
+
+
